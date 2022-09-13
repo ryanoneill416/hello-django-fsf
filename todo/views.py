edcabd38a612717bc93401d4b3bbad2756c1ea91
Ryan O'Neill <ryanoneill416@gmail.com>
2022-09-13 20:46:55 +0000

    Successfully implement comprehensive testing

--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ from .forms import ItemForm
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
-        'items' : items
+        'items': items
     }
     return render(request, 'todo/todo_list.html', context)
 
